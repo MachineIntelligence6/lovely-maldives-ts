67cba82123c6cf211e64bb5f2699deb9f80d499e
Ali Muhammad <jb.alimuhammad@gmail.com>
2024-03-25 12:59:10 +0500

    conflict resolved blogs post

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,5 +1,34 @@
-function AboutUsPage() {
-  return <div>About us</div>
+'use client'
+
+import { Box } from '@mui/system'
+// import { Button } from '@mui/material'
+import { signIn } from 'next-auth/react'
+import { auth } from '@/config/auth'
+
+async function AboutUsPage() {
+  const session = await auth()
+  return (
+    <div>
+      About us.
+      {session?.user ? (
+        <>Welcome {session?.user?.name}</>
+      ) : (
+        <Box>
+          <button
+            type="button"
+            onClick={() =>
+              signIn('Credentials', {
+                email: 'jsmith@example.com',
+                password: 'asdasd',
+              })
+            }
+          >
+            Login
+          </button>
+        </Box>
+      )}
+    </div>
+  )
 }
 
 export default AboutUsPage
--- src/app/api/blogs/post.model.ts
+++ src/app/api/blogs/post.model.ts
@@ -3,12 +3,12 @@
     title: string
     slug: string
     description: string
-    author: ref User/Author
-    images Image[]
     tags: ref Tag[]
     categories: ref Category[]
     publishedAt: timestamp
     featured: boolean
     content: richtext
+    author: ref User/Author
+    images Image[]
     comments: ref Comment
 */
