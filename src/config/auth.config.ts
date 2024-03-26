2dfee76d20dec823de7d02faf6796efefcdfbf6e
Ali Muhammad <jb.alimuhammad@gmail.com>
2024-03-26 12:09:25 +0500

    authjs custom pages

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -32,4 +32,11 @@ export default {
       },
     }),
   ],
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-email', // (used for check email message)
+    newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
 } satisfies NextAuthConfig
