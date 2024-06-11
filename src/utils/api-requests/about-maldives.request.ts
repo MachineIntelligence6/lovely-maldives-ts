5080c36a48ef547c0ba819ed8627804dbcac60bb
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2024-06-11 17:54:25 +0500

    deployment issue resolving.

--- src/utils/api-requests/about-maldives.request.ts
+++ src/utils/api-requests/about-maldives.request.ts
@@ -7,7 +7,7 @@ interface AboutMaldivesShort {
 }
 
 export const aboutMaldivesShortRequest = async (data: AboutMaldivesShort) => {
-  return apiClient.post('api/home/about-maldives-short', {
+  return apiClient.post('/home/about-maldives-short', {
     title: data.title,
     description: data.description,
     homeBgId: data.homeBgId,
@@ -15,5 +15,5 @@ export const aboutMaldivesShortRequest = async (data: AboutMaldivesShort) => {
 }
 
 export const getAboutMaldivesShortRequest = async () => {
-  return apiClient.get('api/home/about-maldives-short')
+  return apiClient.get('/home/about-maldives-short')
 }
