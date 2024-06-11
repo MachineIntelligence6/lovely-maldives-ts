692b1248095336ea27b7afe9f3982e7aeec68f71
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2024-06-11 19:03:26 +0500

    dep issues

--- src/services/homeSections.ts
+++ src/services/homeSections.ts
@@ -1,17 +1,20 @@
 import prisma from '../../prisma'
 
 export async function getHomeSections() {
-  return prisma.homeBg.findFirst({
-    include: {
-      aboutMaldivesShort: true,
-      services: true,
-      aboutUsShort: true,
-      wonders: true,
-      collections: true,
-      brands: true,
-      socialLinkSection: true,
-      header: true,
-      footer: true,
-    },
+  console.log('before fetching')
+  const result = await prisma.homeBg.findFirst({
+    // include: {
+      // aboutMaldivesShort: true,
+      // services: true,
+      // aboutUsShort: true,
+      // wonders: true,
+      // collections: true,
+      // brands: true,
+      // socialLinkSection: true,
+      // header: true,
+      // footer: true,
+    // },
   })
+  console.log('after fetching =>>> ', result)
+  return result
 }
