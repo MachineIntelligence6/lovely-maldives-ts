083693c1095d428f686883b6d9b6a776ecfba945
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2024-06-11 16:42:59 +0500

    latest changes

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 console.log('API Base URL:', process.env.NEXT_PUBLIC_API_BASE_URL)
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://lovely-maldives-ts.vercel.app/api',
   timeout: 60000,
 })
 
