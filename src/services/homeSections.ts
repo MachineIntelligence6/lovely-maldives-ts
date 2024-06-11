3a22496baa2bd004181747455b5af0e954d236e0
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2024-06-11 19:14:36 +0500

    deployment issue resolving

--- src/services/homeSections.ts
+++ src/services/homeSections.ts
@@ -3,17 +3,17 @@ import prisma from '../../prisma'
 export async function getHomeSections() {
   console.log('before fetching')
   const result = await prisma.homeBg.findFirst({
-    // include: {
-      // aboutMaldivesShort: true,
-      // services: true,
-      // aboutUsShort: true,
-      // wonders: true,
-      // collections: true,
-      // brands: true,
-      // socialLinkSection: true,
-      // header: true,
-      // footer: true,
-    // },
+    include: {
+      aboutMaldivesShort: true,
+      services: true,
+      aboutUsShort: true,
+      wonders: true,
+      collections: true,
+      brands: true,
+      socialLinkSection: true,
+      header: true,
+      footer: true,
+    },
   })
   console.log('after fetching =>>> ', result)
   return result
