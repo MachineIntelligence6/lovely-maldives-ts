061a7252e81082d2cb56e6af0791d8143ed01204
ansar695 <ansarsaeed988@gmail.com>
2025-01-15 17:27:30 +0500

    latest code

--- src/admin-components/common/ReactQuillEditor.tsx
+++ src/admin-components/common/ReactQuillEditor.tsx
@@ -1,117 +1,124 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/no-extraneous-dependencies */
+// /* eslint-disable import/no-unresolved */
+// /* eslint-disable import/no-extraneous-dependencies */
 
-'use client'
+// 'use client'
 
-import React, { useEffect, useRef } from 'react'
-import ReactQuill, { Quill } from 'react-quill'
-import 'react-quill/dist/quill.snow.css';
-import ImageResize from 'quill-image-resize-module-react'
+// import React, { useEffect, useRef } from 'react'
+// import ReactQuill, { Quill } from 'react-quill'
+// import 'react-quill/dist/quill.snow.css';
+// import ImageResize from 'quill-image-resize-module-react'
 
-Quill.register('modules/imageResize', ImageResize)
+// Quill.register('modules/imageResize', ImageResize)
 
-const Font = ReactQuill.Quill.import('formats/font')
-Font.whitelist = ['large', 'medium', 'small', 'regular', 'bold', 'pullquote']
+// const Font = ReactQuill.Quill.import('formats/font')
+// Font.whitelist = ['large', 'medium', 'small', 'regular', 'bold', 'pullquote']
 
-ReactQuill.Quill.register(Font, true)
+// ReactQuill.Quill.register(Font, true)
 
-const ReactQuillEditor = (props: any) => {
-  const { height, handleEditorValue, value } = props
-  const quillRef = useRef(null)
+// const ReactQuillEditor = (props: any) => {
+//   const { height, handleEditorValue, value } = props
+//   const quillRef = useRef(null)
 
-  const modules = {
-    toolbar: [
-      [{ header: '1' }, { header: '2' }, { font: [] }],
-      [{ size: [] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [
-        {
-          color: ['#F00', '#0F0', '#00F', '#000', '#FFF', 'color-picker'],
-        },
-      ],
-      [{ align: [] }],
-      [
-        { list: 'ordered' },
-        { list: 'bullet' },
-        { indent: '-1' },
-        { indent: '+1' },
-      ],
-      ['link', 'image', 'video'],
-      ['clean'],
-    ],
-    clipboard: {
-      matchVisual: false,
-    },
-    imageResize: {
-      parchment: ReactQuill.Quill.import('parchment'),
-      modules: ['Resize', 'DisplaySize'],
-    },
-  }
+//   const modules = {
+//     toolbar: [
+//       [{ header: '1' }, { header: '2' }, { font: [] }],
+//       [{ size: [] }],
+//       ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+//       [
+//         {
+//           color: ['#F00', '#0F0', '#00F', '#000', '#FFF', 'color-picker'],
+//         },
+//       ],
+//       [{ align: [] }],
+//       [
+//         { list: 'ordered' },
+//         { list: 'bullet' },
+//         { indent: '-1' },
+//         { indent: '+1' },
+//       ],
+//       ['link', 'image', 'video'],
+//       ['clean'],
+//     ],
+//     clipboard: {
+//       matchVisual: false,
+//     },
+//     imageResize: {
+//       parchment: ReactQuill.Quill.import('parchment'),
+//       modules: ['Resize', 'DisplaySize'],
+//     },
+//   }
 
-  const formats = [
-    'header',
-    'font',
-    'size',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-    'video',
-    'color',
-    'background',
-    'align',
-  ]
+//   const formats = [
+//     'header',
+//     'font',
+//     'size',
+//     'bold',
+//     'italic',
+//     'underline',
+//     'strike',
+//     'blockquote',
+//     'list',
+//     'bullet',
+//     'indent',
+//     'link',
+//     'image',
+//     'video',
+//     'color',
+//     'background',
+//     'align',
+//   ]
 
-  useEffect(() => {
-    const quill = (quillRef.current as unknown as ReactQuill)?.getEditor()
-    const toolbar = quill?.getModule('toolbar')
+//   useEffect(() => {
+//     const quill = (quillRef.current as unknown as ReactQuill)?.getEditor()
+//     const toolbar = quill?.getModule('toolbar')
 
-    const showColorPicker = (val: any) => {
-      if (val === 'color-picker') {
-        const picker = document.createElement('input')
-        picker.type = 'color'
-        picker.style.display = 'none'
-        picker.value = '#FF0000'
-        document.body.appendChild(picker)
+//     const showColorPicker = (val: any) => {
+//       if (val === 'color-picker') {
+//         const picker = document.createElement('input')
+//         picker.type = 'color'
+//         picker.style.display = 'none'
+//         picker.value = '#FF0000'
+//         document.body.appendChild(picker)
 
-        picker.addEventListener(
-          'change',
-          () => {
-            quill.format('color', picker.value)
-          },
-          false
-        )
+//         picker.addEventListener(
+//           'change',
+//           () => {
+//             quill.format('color', picker.value)
+//           },
+//           false
+//         )
 
-        picker.click()
-      } else {
-        quill.format('color', val)
-      }
-    }
+//         picker.click()
+//       } else {
+//         quill.format('color', val)
+//       }
+//     }
 
-    toolbar.addHandler('color', showColorPicker)
-  }, [])
+//     toolbar.addHandler('color', showColorPicker)
+//   }, [])
 
-  return (
-    <ReactQuill
-      theme="snow"
-      ref={quillRef}
-      value={value}
-      onChange={(val: any) => {
-        // setValue(val)
-        handleEditorValue(val)
-      }}
-      style={{ height: height || '250px' }}
-      modules={modules}
-      formats={formats}
-      bounds="#root"
-    />
-  )
+//   return (
+//     <ReactQuill
+//       theme="snow"
+//       ref={quillRef}
+//       value={value}
+//       onChange={(val: any) => {
+//         // setValue(val)
+//         handleEditorValue(val)
+//       }}
+//       style={{ height: height || '250px' }}
+//       modules={modules}
+//       formats={formats}
+//       bounds="#root"
+//     />
+//   )
+// }
+
+// export default ReactQuillEditor
+import React from 'react'
+
+const ReactQuillEditor = () => {
+  return <div>test</div>
 }
 
 export default ReactQuillEditor
